2e538b1de238d7a78c764c99e4fc05141d967c18
Angelica Miranda <am@MacBook-Air-de-Angelica.local>
2020-04-13 14:25:29 -0300

    count

--- day00/ex03/count.py
+++ day00/ex03/count.py
@@ -6,7 +6,7 @@
 #    By: anolivei <anolivei@student.42sp.org>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/04/13 11:01:02 by anolivei          #+#    #+#              #
-#    Updated: 2020/04/13 14:17:33 by anolivei         ###   ########.fr        #
+#    Updated: 2020/04/13 14:24:30 by anolivei         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,12 +19,12 @@ def text_analyzer(text = None):
         lower = sum(c.islower() for c in text)
         punct = sum((c in string.punctuation) for c in text)
         space = sum(c.isspace() for c in text)
-
+    
     print(f"The text contains {len(text)} characters:\n\n"
-            f"- {upper} upper letters\n\n"
-            f"- {lower} lower letters\n\n"
-            f"- {punct} punctuation marks\n\n"
-            f"- {space} spaces")
-
+          f"- {upper} upper letters\n\n"
+          f"- {lower} lower letters\n\n"
+          f"- {punct} punctuation marks\n\n"
+          f"- {space} spaces")
+          
 if __name__ == '__main__':
     text_analyzer()
