d5802ef5bbe6fe0b96356efc909d336669490c5e
Angelica Miranda <am@MacBook-Air-de-Angelica.local>
2020-04-13 09:04:42 -0300

    exec

--- day00/ex01/exec.py
+++ day00/ex01/exec.py
@@ -6,8 +6,18 @@
 #    By: anolivei <anolivei@student.42sp.org>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/04/13 07:44:10 by anolivei          #+#    #+#              #
-#    Updated: 2020/04/13 07:44:12 by anolivei         ###   ########.fr        #
+#    Updated: 2020/04/13 09:03:04 by anolivei         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+import sys
 
+def function():
+	phrase = sys.argv
+	del phrase[0]
+	phrase = ' '.join(phrase)
+	phrase = phrase.swapcase()
+	print(phrase[::-1])
+
+if __name__ == '__main__':
+    function()
